96fa1ca8022603abd3890df2ff0000921d663aab
Ingo Rockel <ingo.rockel@gmail.com>
2016-07-12 14:13:17 +0200

    raised vm limit to 512m

--- tda/bin/tda.bat
+++ tda/bin/tda.bat
@@ -4,4 +4,4 @@ rem Basic start script for TDA for Windows
 rem ***********
 rem If you have big log file you might need to adjust Xmx setting
 
-java -Xmx256m -jar "%~dp0/../tda.jar"
+java -Xmx512m -jar "%~dp0/../tda.jar"
--- tda/bin/tda.sh
+++ tda/bin/tda.sh
@@ -2,4 +2,4 @@
 # Unix shell script for starting tda. If you have big log files
 # you might need to adjzst Xmx setting.
 
-cd ..; java -Xmx256m -jar ./tda.jar 
+cd ..; java -Xmx512m -jar ./tda.jar 
